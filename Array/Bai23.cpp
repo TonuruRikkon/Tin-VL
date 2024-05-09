2af5501f8ff2b638324418ff3313075fa6c5d1bb
TonuruRikkon <dminhphu0303@gmail.com>
2024-05-09 08:38:19 +0700

    adding file

---
+++ Array/Bai23.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,d=0,y=0,dem[1000],res=0;
+    cin>>n;
+    double a[1000];
+    string s[1000];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    string x="",result="";
+    for(int i=0;i<n;i++){
+        if(a[i]>0){
+            x+=to_string(a[i])+',';
+            d++;
+        }
+        else{
+            s[y]=x;
+            dem[y]=d;
+            y++;
+            x="";
+            d=0;
+        }
+    }
+    if(a[n-1]>0){
+        s[y]=x;
+        dem[y]=d;
+    }
+    for(int i=0;i<=y;i++){
+        if(dem[i]==*max_element(dem,dem+y)){
+            res++;
+            result+=s[i]+";";
+        }
+    }
+    cout<<res<<endl<<result;
+}
