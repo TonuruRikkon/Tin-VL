b86a5e8ed94df73fd0ae8e36420b67b9050c5281
phupph <phupph123@gmail.com>
2024-05-11 13:19:03 +0700

    adding file

--- Array2D.cpp/mang2chieu/BT5.cpp
+++ Array2D.cpp/mang2chieu/BT5.cpp
@@ -3,9 +3,10 @@ using namespace std;
 
 int main()
 {
-    int n,m;
+    int n,m,check,d=0;
     cin>>n>>m;
-    int a[n][m];
+    int a[n][m],b[n*m];
+    memset(b,0,n*m);
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < m; j++)
@@ -18,9 +19,25 @@ int main()
     {
         for (int j = 0; j < m; j++)
         {
-            
+            for (int k = 2; k < a[i][j]; k++)
+            {
+                if(a[i][j]%k==0) {
+                    check=0;
+                    break;
+                }
+                else check=1; 
+            }
+            if(check==1){
+                b[d]=a[i][j];
+                d++;
+                check=0;
+            }
         }
-        
     }
+    for (int i = 0; i < d; i++)
+    {
+        cout<<"["<<b[i]<<"]";
+    }
+    cout<<endl<<"So luong so nguyen to la:"<<d;
     
 }
