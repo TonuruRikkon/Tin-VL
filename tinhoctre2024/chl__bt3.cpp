f707fbcd152e09cb992cc1d9d5c5bbf84c90d625
TonuruRikkon <dminhphu0303@gmail.com>
2024-08-01 11:00:17 +0700

    adding file

---
+++ tinhoctre2024/chl__bt3.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,minn,maxx=0,rt=0,d=0,wt=0;
+    cin>>n;
+    pair<int,int> bo[5000+1];
+    vector<pair<int,int>> c;
+    vector<pair<int,int>> m;
+    for (int i = 0; i < n; i++)
+    {
+        cin>>bo[i].first>>bo[i].second;
+    }
+    sort(bo,bo+n);
+    minn=bo[0].first;
+    for (int i = 1; i < n; i++)
+    {
+        int x;
+        if(bo[i].first<=bo[i-1].second) x=bo[i].second;
+        else{
+            c.push_back(make_pair(minn,x));
+            minn=bo[i].first;
+            m.push_back(make_pair(bo[i].first,bo[i-1].second));
+            x=bo[i].second;
+            d++;
+        }
+    }
+    for (int i = 0; i < d; i++)
+    {
+        if(c[i].second-c[i].first>wt)wt=c[i].second-c[i].first;
+        if(m[i].first-m[i].second>rt) rt=m[i].first-m[i].second;
+    }
+    cout<<wt<<" "<<rt;
+    
+}
