26987bcff5af7d7208aff1cb07862153dc063be8
TonuruRikkon <dminhphu0303@gmail.com>
2024-08-03 10:15:28 +0700

    adding file

--- tinhoctre2024/baitapsach/trchoi2.cpp
+++ tinhoctre2024/baitapsach/trchoi2.cpp
@@ -1,11 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 bool check(string s,int L,int R){
-    for (int i = L; i <= (L+R)/2; i++)
+    string x,y;
+    for (int i = L; i <= R; i++)
     {
-        if(s[i]!=s[L+R-i]) return false;
+        x.push_back(s[i]);
     }
-    return true;
+    y=x;
+    reverse(y.begin(),y.end());
+    if(x==y) return true;
+    else return false;
 }
 int main()
 {
@@ -19,6 +23,9 @@ int main()
         int x,y;
         cin>>x>>y;
         test.push_back(make_pair(x-1,y-1));
+    }
+    for(auto i:test){
+        cout<<i.first<<" "<<i.second<<"!"<<endl;
     }
     for(auto i:test){
         if(check(s,i.first,i.second)==true) cout<<1<<endl;
