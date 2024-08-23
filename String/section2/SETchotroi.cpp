2a4cf8ebc9bd507e3897a63fae50f3ce0c573a5c
phupph <phupph123@gmail.com>
2024-08-24 01:17:29 +0700

    adding file

--- String/section2/SETchotroi.cpp
+++ String/section2/SETchotroi.cpp
@@ -15,17 +15,18 @@ bool kiemtra(string x,string y, string z)
 
 int main()
 {
-    int n;
+    int n,ans=0;
     cin>>n;
     cin.ignore();
     for (int i = 0; i < n; i++)
     {
         string str;
         cin>>a[i].amo>>a[i].clo>>a[i].way>>a[i].sap;
+        if(a[i].amo=="one") a[i].sap+='s';
     }
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; i+=3)
     {
-        
+        if(kiemtra(a[i].amo,a[i+1].amo,a[i+3].amo)==true||kiemtra(a[i].clo,a[i+1].clo,a[i+3].clo)==true||kiemtra(a[i].way,a[i+1].way,a[i+3].way)==true||kiemtra(a[i].sap,a[i+1].sap,a[i+3].sap)==true) ans++;
     }
-    
+    cout<<ans;
 }
