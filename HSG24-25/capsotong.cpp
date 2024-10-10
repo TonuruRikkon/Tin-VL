6891cceed50345ea702ee76d2eac7a706f10067a
TonuruRikkon <dminhphu0303@gmail.com>
2024-10-10 17:53:53 +0700

    adding file

--- HSG24-25/capsotong.cpp
+++ HSG24-25/capsotong.cpp
@@ -3,5 +3,41 @@ using namespace std;
 
 int main()
 {
-    
+    int n,k,outt=0;
+    cin>>n;
+    cin.ignore();
+    string str,temp="";
+    map<int,int> ans;
+    getline(cin,str);
+    cin>>k;
+    for (int i = 0; i <= str.size(); i++)
+    {
+        if(isdigit(str[i])||str[i]=='-'&&i!=str.size()){
+            temp+=str[i];
+        }
+        else
+        {
+            if(!temp.empty())
+            {
+                ans[stoi(temp)]++;
+                temp.clear();
+            }
+        }
+    }
+    if(!temp.empty()){
+        ans[stoi(temp)]++;
+    }
+    for(auto x:ans){
+        int ref=x.first;
+        int y=k-ref;
+        if(ans.find(y)!=ans.end()){
+            if(ref+y==k){
+                if(ref==y) outt+=(x.second*(x.second-1))/2; 
+                else if(ref>y){
+                outt+=x.second*ans[y];
+                }
+            } 
+        }
+    }
+    cout<<outt;
 }
--- testall.cpp
+++ testall.cpp
@@ -1,14 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    int se=0;
-    string s; cin>>s;
-    for(int i=0;i<s.size();i++){
-        if(s[i]==s[i+1]) se+=2;
-        if(s[i]=='a' or s[i]=='d' or s[i]=='g' or s[i]=='j' or s[i]=='m' or s[i]=='p' or s[i]=='t' or s[i]=='w') se++;
-        if(s[i]=='b' or s[i]=='e' or s[i]=='h' or s[i]=='k' or s[i]=='n' or s[i]=='q' or s[i]=='u' or s[i]=='x') se+=2;
-        if(s[i]=='c' or s[i]=='f' or s[i]=='i' or s[i]=='l' or s[i]=='o' or s[i]=='r' or s[i]=='v' or s[i]=='y') se+=3;
-        if(s[i]=='s' or s[i]=='z') se+=4;
-        cout<<s[i]<<"."<<se<<endl;
-    }cout<<se;
+    string a="123";
+    cout<<a.size();
 }
