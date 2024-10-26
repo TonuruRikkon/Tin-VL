1be10ddf20e248d732815675552bf62300dc035f
TonuruRikkon <dminhphu0303@gmail.com>
2024-10-26 11:45:33 +0700

    adding file

--- DeminhhoaBangB2024/cstring.cpp
+++ DeminhhoaBangB2024/cstring.cpp
@@ -6,13 +6,20 @@ int main()
     string str1,str2;
     cin>>str1>>str2;
     int sizeo=str1.size(),j=0,d=0;
-    for(char x:str2){
-        if(x==str1[j]){
-            j++;
+    if(str2.size()>int(1e3)){
+        for(char x:str2){
+            if(x==str1[j]){
+                j++;
+            }
+            if(j==sizeo){
+                d++;
+                j=0;
+            }
         }
-        if(j==sizeo){
-            d++;
-            j=0;
+    }
+    else{
+        while(str2.find(str1)!=-1){
+            str2.erase(str2.find(str1),str1.size());
         }
     }
     cout<<d;
