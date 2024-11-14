4b0fc45b18cd77941d5c466b73ab84ec7b49f41e
phupph <136301662+phupph@users.noreply.github.com>
2024-11-14 13:23:11 +0700

    adding file

--- luyende2024/BinhDuong21/BUSINESS.cpp
+++ luyende2024/BinhDuong21/BUSINESS.cpp
@@ -1,7 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+vector<int> inp[int(1e6)];
+
 int main()
 {
+    int n,m;
+    cin>>n>>m;
+    for (int i = 0; i < m; i++)
+    {
+        int x,y;
+        cin>>x>>y;
+        inp[x].push_back(y);
+        inp[y].push_back(x);
+    }
     
 }
