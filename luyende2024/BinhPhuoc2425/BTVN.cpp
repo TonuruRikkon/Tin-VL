0736d40e4c7d969a9db17999014899a7a290a124
TonuruRikkon <dminhphu0303@gmail.com>
2024-11-19 18:23:14 +0700

    adding file

--- luyende2024/BinhPhuoc2425/BTVN.cpp
+++ luyende2024/BinhPhuoc2425/BTVN.cpp
@@ -1,41 +1,60 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool ngto[int(1e6)+1];
-int uoc[int(1e6)+1];
+bool ngto[int(1e7)];
+vector<int> ngt;
 
 void sangnt()
 {
-    memset(ngto,false,sizeof(ngto));
-    memset(uoc,0,sizeof(uoc));
+    memset(ngto,true,sizeof(ngto));
     ngto[0]=ngto[1]=false;
-    uoc[1]=1;
     int p=2;
-    while (p*p<=int(1e6))
+    while (p*p<int(1e7))
     {
-        for (int i = 0; i < int(1e6)/p; i++)
+        ngt.push_back(p);
+        for (int i = 2; i*i < int(1e7); i++)
         {
-            
+            ngto[i*p]=false;
         }
-        
+        for(p++;p*p<int(1e7)&&ngto[p]==false;p++);
     }
-    
 }
 
 int main()
 {
+    sangnt();
     int t;
     cin>>t;
+    vector<long long> ans;
     for (int i = 0; i < t; i++)
     {
+        long long outp=1;
         int x;
-        long long tol=1;
         cin>>x;
-        for (int i = 2; i <= x; i++)
-        {
-            tol*=i;
+        ans.erase(ans.begin(),ans.end());
+        for(int v:ngt){
+            long long u=v;
+            if(v>x) break;
+            else{
+                int d=0;
+                long long num=1,tol=1;
+                while(u<=x){
+                    d+=x/u;
+                    u*=u;
+                }
+                for (int i = 1; i <= d; i++)
+                {
+                    num*=v;
+                    tol+=num;
+                }
+                ans.push_back(tol);
+            }
         }
-        cout<<tol<<endl;
+        for(long long x:ans){
+            outp*=x;
+        }
+        cout<<outp<<endl;
     }
+
     
 }
