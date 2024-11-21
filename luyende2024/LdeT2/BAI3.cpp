f1e8608557be66d85322e5ffb7475147997717c3
TonuruRikkon <dminhphu0303@gmail.com>
2024-11-21 19:13:00 +0700

    adding file

--- luyende2024/LdeT2/BAI3.cpp
+++ luyende2024/LdeT2/BAI3.cpp
@@ -1,12 +1,51 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int m,n,k;
+vector<vector<int>> inp(201,vector<int>(int(201))),tol(201,vector<int>(201,0));
+
+bool can_buy(int s)
+{
+    for (int i = 1; i <= m; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            for (int x = i; x <= m; x++)
+            {
+                for (int y = j; y <= n; y++)
+                {
+                    int tol_cost = tol[x][y] - tol[i-1][y] - tol[x][j-1] + tol[i-1][j-1]; 
+                    int ans= (x-i+1)*(y-j+1);
+                    if(tol_cost<=k&&ans>=s) return true; 
+                }  
+            }
+        }
+    }    
+    return false;
+}
+
+int ong_phu_nhiphan(){
+    int L=1, R=m*n;
+    int ans=0;
+
+    while (L<=R)
+    {
+        int mid=L+(R-L)/2;
+        if(can_buy(mid)){
+            ans = mid;
+            L=mid+1;
+        }
+        else{
+            R=mid-1;
+        }
+    }
+    return ans;
+}
+
 int main()
 {
-    int m,n,k,ans=0;
     cin>>m>>n;
     cin>>k;
-    vector<vector<int>> inp(m,vector<int>(n)),tol(m+1,vector<int>(n+1,0));
     for (int i = 0; i < m; i++)
     {
         for (int j = 0; j < n; j++)
@@ -21,20 +60,5 @@ int main()
             tol[i][j]=inp[i-1][j-1]+tol[i-1][j]+tol[i][j-1]-tol[i-1][j-1];
         }
     }
-    for (int i = 1; i <= m; i++)
-    {
-        for (int j = 1; j <= n; j++)
-        {
-            for (int x = 1; x <= i; x++)
-            {
-                for (int y = 1; y <= j; y++)
-                {
-                    if(tol[i][j]-tol[x][y]<=k){
-                        ans=max(ans,(i-x+1)*(j-y+1));
-                    }
-                }   
-            }
-        }
-    }
-    cout<<ans;
+    cout<<ong_phu_nhiphan();
 }
--- luyende2024/LdeT2/BAI4.cpp
+++ luyende2024/LdeT2/BAI4.cpp
@@ -1,15 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-bool visited[int(1e6)+1];
 vector<int> tso(int(1e6)+1,0),inp[int(1e6)+1];
+int ans=0;
 
-void bfs(int s)
-{
-    memset(visited,false,sizeof(visited));  
-    queue<int> q;
-    visited[s]=true;
-    
+void dfs(int s, int father, int minn,int d){
+    int u=s;
+    for(int v:inp[u]){
+        if(v!=father){
+            int minn2=min(tso[v],minn);
+            int d2=d+1;
+            ans=max(ans,d2*minn2);
+            dfs(v,u,minn2,d2);            
+        }
+    }
 }
 
 int main()
@@ -27,5 +31,9 @@ int main()
         inp[u].push_back(v);
         inp[v].push_back(u);
     }
-    
+    for (int i = 1; i <= n; i++)
+    {
+        dfs(i,-1,tso[i],0);
+    }
+    cout<<ans;
 }
