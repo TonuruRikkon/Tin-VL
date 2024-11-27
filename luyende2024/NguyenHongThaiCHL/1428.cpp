4af383d269e5dbe9169896fc236a3b8d944a9e8a
unknown <phupph>
2024-11-27 15:43:30 +0700

    adding file and fix Bai2(de PHU YEN 2021)

---
+++ luyende2024/NguyenHongThaiCHL/1428.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<pair<int,int>> inp[int(1e6)+1];
+bool vis[int(1e6)+1];
+int steps[int(1e6)+1],from[int(1e6)+1];
+
+void Dijstra(pair<int,int> s)
+{
+    memset(vis,false,sizeof(vis));
+    memset(steps,0,sizeof(steps));
+    memset(from,0,sizeof(from));
+    priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> q;
+    q.push(s);
+    vis[s.second]=true;
+    while (q.empty()==false)
+    {
+        auto topp=q.top();
+        int u=topp.second;
+        vis[topp.second]=true;
+        q.pop();
+        for(auto v:inp[u]){
+            if(vis[v.second]==false){
+                q.push(v);
+                steps[v.second]=steps[u]+v.first;
+                from[v.second]=u;
+            }
+        }
+    }
+    
+}
+
+int main()
+{
+    vector<char> bo;
+    int p,ans=int(1e9);
+    char bonhat=0;
+    cin>>p;
+    for (int i = 0; i < p; i++)
+    {
+        char u,v;
+        int c;
+        cin>>u>>v>>c;
+        if(u>='A'&&u<='Y') bo.push_back(u);
+        else if(v>='A'&&v<='Y') bo.push_back(v);
+        inp[u].push_back({c,v});
+        inp[v].push_back({c,u});
+    }
+    Dijstra({0,'Z'});
+    for(char x:bo){
+        if(ans>steps[x]){
+            ans=steps[x];
+            bonhat=x;
+        }
+    }
+    cout<<bonhat<<" "<<ans;
+}
--- luyende2024/PhuYen2021/BAI3.cpp
+++ luyende2024/PhuYen2021/BAI3.cpp
@@ -6,15 +6,25 @@ int main()
     int n,ans=0;
     cin>>n;
     map<int,int> mp;
+    vector<int> giatri;
     for (int i = 0; i < n; i++)
     {
         int t,m;
         cin>>t>>m;
+        giatri.push_back(m);
         mp[t]=max(mp[t],m);
     }
+    int d=1;
+    vector<int> outp(n,0);
     for (auto x:mp)
     {
+        int it=find(giatri.begin(),giatri.end(),x.second)-giatri.begin();
+        outp[it]=d++;
         ans+=x.second;
+    }
+    for(int x:outp){
+        if(x==0) cout<<d++<<endl;
+        else cout<<x<<endl;
     }
     cout<<ans;
 }
