a8f2b182e12c3906d0fc6b4f3ea23f4a0ca0230d
TonuruRikkon <dminhphu0303@gmail.com>
2024-11-27 18:48:31 +0700

    adding file

--- luyende2024/PhuYen2021/BAI2-03.cpp
+++ luyende2024/PhuYen2021/BAI2-03.cpp
@@ -6,19 +6,14 @@ int dy[4]{0,0,-1,1};
 bool vis[int(256)][int(256)];
 vector<string> inp;
 
-void dfs(int x,int y,int n,int m)
+void dfs(int x,int y)
 {
     vis[x][y]=true;
     for (int i = 0; i < 4; i++)
     {
         int crtX=x+dx[i];
         int crtY=y+dy[i];
-        if(crtX<m&&crtX>=0&&crtY<n&&crtY>=0)
-        {
-            if(inp[crtX][crtY]=='x'&&vis[crtX][crtY]==false){
-                dfs(crtX,crtY,n,m);
-            }
-        }
+        if(inp[crtX][crtY]=='x'&&vis[crtX][crtY]==false) dfs(crtX,crtY);
     }
     
 }
@@ -27,7 +22,6 @@ int main()
 {
     freopen("BAI2.INP","r",stdin);
     freopen("BAI2.OUT", "w",stdout);
-    memset(vis,false,sizeof(vis));
     int d=0;
     inp.push_back("00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000");
     string str;
@@ -38,10 +32,9 @@ int main()
     for(int i=1;i<inp.size();i++){
         for(int j=1;j<inp[i].size();j++){
             if(inp[i][j]=='x'&&vis[i][j]==false){
-                dfs(i,j,inp[i].size(),inp.size());
+                dfs(i,j);
                 ans++;
             }
         }
     }
-    cout<<ans;
 }
