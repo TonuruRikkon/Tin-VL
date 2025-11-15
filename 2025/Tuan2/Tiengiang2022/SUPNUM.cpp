135653625b2cf95b8ee1dc40c05b86db62736f22
TonuruRikkon <dminhphu0303@gmail.com>
2025-11-15 15:21:35 +0700

    adding flies

---
+++ 2025/Tuan2/Tiengiang2022/SUPNUM.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<bool> IsNto(INT_MAX,true);
+vector<int> Nto;
+
+void sangnt()
+{
+    IsNto[0]=IsNto[1]=false;
+    for (int i = 2; i*i <= int(1e5); i++)
+    {
+        if(IsNto[i]==true){
+            Nto.push_back(i);
+            for (int j = i*i; j <= int(1e5); j+=i)
+            {
+                IsNto[j]=false;
+            }
+        }
+    }
+    
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    sangnt();
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin>>x;
+        int ref=0;
+        for(int y:Nto){
+            if(x%y==0){
+                ref+=y;
+            }
+            if(y>x) break;
+        }    
+        if(ref%5==0) cout<<x<<' ';
+    }
+    
+}
